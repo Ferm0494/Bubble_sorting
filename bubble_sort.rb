9344f354a08c25dbe233b099690e33ac8465c5bb
Fernando Rivas <ferm0494@Fernandos-MacBook-Pro.local>
2020-05-07 09:25:55 -0600

    bubble_sort#1

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,1 +1,14 @@
-print 'Hello world!'
+def bubble_sort(array=[4,2,1,7])
+for i in (0 ... array.size)
+    for k in (0 ... array.size)
+        if k+1 != array.size && array[k] > array[k+1] 
+            aux = array[k+1] #2 -->aux
+            array[k+1]= array[k] #1--> array[k+1]
+            array[k]=aux #2 --> array[k]
+        end
+    end
+end
+return array        
+end
+
+print bubble_sort(['z','e','f','a'])
